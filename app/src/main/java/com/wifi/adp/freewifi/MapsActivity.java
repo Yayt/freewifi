b77407d73429a26f279169e1d6b4394f47e31b74
Yayt <kevinmarczyk@gmail.com>
2015-11-12 14:39:21 +0200

    listview kind of works with own layout

--- app/src/main/java/com/wifi/adp/freewifi/MapsActivity.java
+++ app/src/main/java/com/wifi/adp/freewifi/MapsActivity.java
@@ -471,7 +471,7 @@ public class MapsActivity extends FragmentActivity {
                 "Android Example List View"
         };
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, android.R.id.text1, values);
+                R.layout.listitem, android.R.id.text1, values);
 
         listView.setAdapter(adapter);
         adapter.notifyDataSetChanged();
